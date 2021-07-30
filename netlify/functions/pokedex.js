cdbd6ba88a1d5902cf24b05f893b55e4998fc08c
Kristen Lavavej <lavavej@gmail.com>
2021-07-30 15:02:32 -0700

    logging

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,11 +1,17 @@
 const fetch = require('node-fetch')
+const console = require('console')
 
-exports.handler = async function () {
+exports.handler = async function (event, context) {
     const POKE_API = "https://pokeapi.co/api/v2/pokedex/kanto"
+
+    console.log(event)
+
     const response = await fetch(POKE_API)
     const data = await response.json()
     return {
         statusCode: 200,
-        body: JSON.stringify(data)
+        body: JSON.stringify({
+            pokemon: data.pokemon_entries
+        })
     }
 }
