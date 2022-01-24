e5d05ab258365418cfbbcb4a7f587a29692f134c
Kristen Lavavej <35638702+klavavej@users.noreply.github.com>
2022-01-24 07:01:58 -0800

    Delete test-scheduled-function.js

--- netlify/functions/test-scheduled-function.js
+++
@@ -1,8 +0,0 @@
-const { schedule } = require("@netlify/functions");
-
-exports.handler = schedule("0 0 * * *", async (event, context) => {
-  console.log("Hello, World!");
-  return {
-    statusCode: 200,
-  };
-});
